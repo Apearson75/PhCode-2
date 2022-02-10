bd00e1f5fa5b588f756211a5f2858450d89ff946
Phoneguytech75 <88546046+Phoneguytech75@users.noreply.github.com>
2022-02-10 19:11:51 +0000

    added variables

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from pprint import pprint
 import re
 import json
 
+#Operator check for variables
+operators = ['send', 'var']
+
 #Get the file
 file = input("Type the name of your file you want to run")
 if file == "":
@@ -19,21 +22,25 @@ for line in lines:
     
     #Print Command
     if operation == "send":
-        try:    
+        if '"' not in line:
             result = line.split("send ")[1]
+            result = variables[result]
             result = re.search('"(.*)"', result)
             print(result.group(1))
-        except AttributeError:
-            print(f"String Error in line {count} on {file.name}") 
+        else:    
+            try:    
+                result = line.split("send ")[1]
+                result = re.search('"(.*)"', result)
+                print(result.group(1))
+            except AttributeError:
+                print(f"String Error in line {count} on {file.name}")
     
     #Variable Command
     elif operation == "var":    
-            variable = line.split("var ")[1]
-            variable_name = variable.split(" ")[0]
-            variable_data = variable.split(f"{variable_name} ")[1]
-            varaible_json = {
-                variable_name: f'{variable_data}'
-            }
-            variables[variable_name] = variable_data
-            json.dumps(variables)
-            print(variables)          
+        variable = line.split("var ")[1]
+        variable_name = variable.split(" ")[0]
+        variable_data = variable.split(f"{variable_name} ")[1]
+
+            
+        variables[variable_name] = variable_data
+        json.dumps(variables)         
